2f9f8ff9be421fb356690499a6f36c015e77a14f
shanakasp <74164029+shanakasp@users.noreply.github.com>
2024-08-07 23:02:09 +0530

    added 8th.js to .

---
+++ 8th.js
@@ -0,0 +1,9 @@
+var str3 = "bus, car, van";
+var try3 = str3.slice(0, 3);
+
+console.log(str3);
+console.log(try3);
+
+var str4 = "Hello world";
+var try4 = str4.replace("world", "Home");
+console.log(try4);
