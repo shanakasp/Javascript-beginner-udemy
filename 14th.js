8971c49283714e4588cbf3ac5d1a3bfef2270e47
shanakasp <74164029+shanakasp@users.noreply.github.com>
2024-08-10 16:47:51 +0530

    added 14th.js to .

---
+++ 14th.js
@@ -0,0 +1,11 @@
+function food() {
+  let food = "pizza";
+  console.log(`food: ${food}`);
+}
+
+function order(quantity, price) {
+  console.log(`Quantity is ${quantity} and Prince is ${price}`);
+}
+
+food();
+order(10, 20);
